9381c53708e125ea84cde371ca6db57f9d42e2bb
M-Shafay <shafay0099@gmail.com>
2024-10-24 10:29:56 +0500

    Make ,OnePiece is Real

--- src/pages/favpage.jsx
+++ src/pages/favpage.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 function Favpage() {
   return (
     <div>
-
+If i want to have a peronalized view then i have to have A jason file for each user
+and i Load that jason here and List down the movies
+    <ul>
+      <script>
+      {/* for(i= 0 i < user movie array size i++)
+          movie[i] */}
+      </script>
+    </ul>
     </div>
   )
 }
